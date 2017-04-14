d02cdbfeb9fb9cb8a617a7f639aa7bf47800d389
fengluyan <466652855@qq.com>
2017-04-14 13:28:09 +0800

    login

--- Xunmi/app/src/main/java/com/sohu/xunmi/login/LoginActivity.java
+++ Xunmi/app/src/main/java/com/sohu/xunmi/login/LoginActivity.java
@@ -2,10 +2,15 @@ package com.sohu.xunmi.login;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.sohu.xunmi.R;
+
 
 /**
  * Created by whisky on 2017/4/14.
@@ -14,19 +19,29 @@ import android.widget.EditText;
 public class LoginActivity extends Activity implements DialogInterface.OnClickListener, View.OnLongClickListener {
     EditText loginName, loginPass;
     Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login_activity);
-        loginName = (EditText)findViewById(R.id.loginName);
-        loginButton = (Button)findViewById(R.id.loginButton);
+        loginName = (EditText) findViewById(R.id.loginName);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginPass = (EditText) findViewById(R.id.loginPassword);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //startActivity(MainActivity.class);
+                Editable userName = loginName.getText();
+                Editable userPassword = loginPass.getText();
+                //push name&password
+                Intent intent = new Intent();
+                intent.setClass(LoginActivity.this, TagIAm.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
+
+
 
 
     public void onClick(DialogInterface dialogInterface, int i) {
--- Xunmi/app/src/main/java/com/sohu/xunmi/login/RegisterActivity.java
+++ Xunmi/app/src/main/java/com/sohu/xunmi/login/RegisterActivity.java
@@ -1,16 +1,13 @@
 package com.sohu.xunmi.login;
 
-import android.app.Activity;
-import android.os.Bundle;
-
 /**
  * Created by whisky on 2017/4/14.
  */
 
-public class RegisterActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_register_activity);
-    }
-}
+//public class RegisterActivity extends Activity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.layout_register_activity);
+//    }
+//}
