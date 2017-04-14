75c80cda072608428cf5a1a2282bf0df6b854e6b
whisky <whisky@MacBook-Pro.local>
2017-04-14 10:53:27 +0800

    login register init

---
+++ Xunmi/app/src/main/java/com/sohu/xunmi/login/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.sohu.xunmi.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.sohu.xunmi.R;
+
+/**
+ * Created by whisky on 2017/4/14.
+ */
+
+public class RegisterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_register_activity);
+    }
+}
